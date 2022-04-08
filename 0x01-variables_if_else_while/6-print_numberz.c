5b95b75f224b16e026ec2d98461e9b4f5738800f
Samuel Mbanaso <Samglor25@gmail.com>
2022-04-08 15:51:55 -0700

    print base 10 numbers followed by a new line

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,18 +2,18 @@
 
 /**
  * main - entry point
- * 
+ *
  * Description: Print numbers 0 to 9 on one line
  * Return: 0
  */
 int main(void)
 {
-	int i;
+int i;
 
-	for (i = 0; i <= 9; ++i)
-		putchar('0' + i);
+for (i = 0; i <= 9; ++i)
+putchar('0' + i);
 
-	putchar('\n');
+putchar('\n');
 
-	return (0);
+return (0);
 }
