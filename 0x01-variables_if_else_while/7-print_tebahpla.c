1c27e6ddc2c9804379e1c60c363661d88c7a75ab
Samuel Mbanaso <Samglor25@gmail.com>
2022-04-08 15:11:00 -0700

    print alphabets backwards

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio>
+
+/**
+ * main - entry point
+ *
+ * Description: Print lowercase alphabet backwards
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c >= 'a'; --c)
+		putchar(c);
+
+	putchar('\n);
+
+	return (0);
+}
